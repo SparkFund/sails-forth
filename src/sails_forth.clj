75a4c5c5cb2b77ab54c8acf7d424ccb73c038c92
Donald Ball <donald.ball@livingsocial.com>
2015-12-28 16:16:17 -0500

    Reimplement authenticated requests

--- src/sails_forth.clj
+++ src/sails_forth.clj
@@ -41,9 +41,11 @@
 
 (t/defn json-request
   [method :- HttpMethod
+   headers :- HttpHeaders
    url :- HttpUrl
    params :- (t/Option HttpParams)] :- HttpResponse
   (let [request (cond-> {:method method
+                         :headers headers
                          :url url
                          :throw-exceptions false
                          :accept :json
@@ -59,54 +61,55 @@
                       :access-token t/Str}
           :complete? true))
 
+(t/defalias Config
+  (t/HMap :mandatory {:username t/Str
+                      :password t/Str
+                      :token t/Str
+                      :consumer-key t/Str
+                      :consumer-secret t/Str}
+          :optional {:version t/Int
+                     :sandbox? t/Bool
+                     :host t/Str}))
+
+(t/defalias State
+  (t/HMap :mandatory {:host t/Str
+                      :authentication (t/Option Authentication)
+                      :version-url (t/Option HttpUrl)
+                      :requests t/Int
+                      :config Config}
+          :complete? true))
+
 (t/defn authenticate
-  [host :- t/Str
-   username :- t/Str
-   password :- t/Str
-   token :- t/Str
-   consumer-key :- t/Str
-   consumer-secret :- t/Str] :- (t/Option Authentication)
-  (let [params {:username username
+  [config :- Config] :- (t/Option Authentication)
+  (let [{:keys [host username password token consumer-key consumer-secret]} config
+        params {:username username
                 :password (str password token)
                 :client_id consumer-key
                 :client_secret consumer-secret
                 :grant_type "password"}
         url (str "https://" host "/services/oauth2/token")
-        response (json-request :post url params)
+        response (json-request :post {} url params)
         {:keys [status body]} response]
     (when (and (= 200 status)
                (map? body))
       (let [{:strs [instance_url access_token]} body]
-        (when (and instance_url access_token)
-          ;; TODO why are the str calls necessary to type check?
-          {:instance-url (str instance_url)
-           :access-token (str access_token)})))))
+        (when (and (string? instance_url)
+                   (string? access_token))
+          {:instance-url instance_url
+           :access-token access_token})))))
 
-(t/defalias VersionUrl HttpUrl)
-
-(t/defn ^:no-check versions
-  [url :- t/Str] :- (t/Option (t/Seq VersionUrl))
+(t/ann ^:no-check versions [t/Str -> (t/Option (t/Seq HttpUrl))])
+(defn versions
+  [url]
   (let [url (str url "/services/data/")
-        response (json-request :get url nil)
+        response (json-request :get {} url nil)
         {:keys [status body]} response]
     (when (and (= 200 status)
                (sequential? body)
                (every? (map map? body)))
       (let [urls (map #(get % "url") body)]
         (when (every? identity urls)
-          urls)))))
-
-(t/defalias State
-  (t/HMap :mandatory {:host t/Str
-                      :authentication (t/Option Authentication)
-                      :version-path (t/Option HttpUrl)
-                      :requests t/Int}
-          :complete? true))
-
-(t/defalias Config
-  (t/HMap :optional {:version t/Int
-                     :sandbox? t/Bool
-                     :host t/Str}))
+         urls)))))
 
 (t/defn derive-host
   [config :- Config] :- t/Str
@@ -117,14 +120,57 @@
   [config :- Config] :- State
   (let [{:keys [version]} config]
     (cond-> {:authentication nil
-             :version-path nil
+             :version-url nil
              :requests 0
-             :host (derive-host config)}
-      version (assoc :version-path (str "/services/data/v" version)))))
+             :host (derive-host config)
+             :config config}
+      version (assoc :version-url (str "/services/data/v" version)))))
+
+(t/defn try-authentication
+  [state :- State] :- State
+  (let [{:keys [authentication config requests]} state]
+    (cond-> state
+      (not authentication)
+      (assoc :authentication (authenticate config)
+             :requests (inc requests)))))
+
+(t/defn try-to-find-latest-version
+  [state :- State] :- State
+  (let [{:keys [authentication requests version-url]} state]
+    (cond-> state
+      (and (not version-url) authentication)
+      (assoc :version-url (last (versions (:instance-url authentication)))
+             :requests (inc requests)))))
 
 (t/defn request
   [state :- State
    method :- HttpMethod
    url :- HttpUrl
-   params :- HttpParams] :- (t/HVec [State HttpResponse])
-  [state {:status 200}])
+   params :- HttpParams] :- (t/HVec [State (t/Option HttpResponse)])
+  (t/loop [state :- State state
+           tries :- t/Int 0]
+    (let [state (-> state
+                    try-authentication
+                    try-to-find-latest-version)
+          {:keys [authentication requests]} state
+          response (when-let [{:keys [access-token]} authentication]
+                     (let [headers {"Authorization" (str "Bearer " access-token)}]
+                       (json-request method headers url params)))
+          {:keys [status body]} response
+          state (cond-> state
+                  authentication
+                  (assoc :requests (inc requests)))]
+      (if (and (= 401 status)
+               (= tries 0))
+        (recur (assoc state :authentication nil) (inc tries))
+        [state response]))))
+
+(t/defn request!
+  [state :- (t/Atom1 State)
+   method :- HttpMethod
+   url :- HttpUrl
+   params :- HttpParams] :- (t/Option HttpResponse)
+  (let [[state' response] (request @state method url params)]
+    ;; TODO could try to merge states, otherwise request count may be incorrect
+    (reset! state state')
+    response))
