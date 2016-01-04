1f6c5c529764ec517b0a5cea623409c627c0f9c2
Donald Ball <donald.ball@livingsocial.com>
2016-01-04 16:09:45 -0500

    Add config and client vars
    Probably not a great thing in the long run, but useful

--- src/user.clj
+++ src/user.clj
@@ -1,3 +1,11 @@
 (ns user
   (:require [clojure.core.typed :as t]
-            [sails-forth :as sf]))
+            [sails-forth :as sf]
+            [sails-forth-test]))
+
+(def config
+  (sails-forth-test/load-config))
+
+(def client
+  (when config
+    (sf/build-client! config)))
