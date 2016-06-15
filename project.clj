2e984cbfb2c0528e9afff0514cd6246d1aacf3d9
Donald Ball <donald.ball@livingsocial.com>
2016-06-15 15:16:37 -0400

    Release 0.2.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject sparkfund/sails-forth "0.1.7"
+(defproject sparkfund/sails-forth "0.2.0"
   :description "A mostly-typed salesforce library"
   :url "http://github.com/sparkfund/sails-forth"
   :license {:name "Eclipse Public License"
