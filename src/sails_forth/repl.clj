d9781efb79583205e005a0e34f2c84fe57112aae
Donald Ball <donald.ball@gmail.com>
2016-07-24 23:05:10 -0400

    Update ns ref

--- src/sails_forth/repl.clj
+++ src/sails_forth/repl.clj
@@ -1,9 +1,9 @@
 (ns sails-forth.repl
   (:require [clojure.core.typed :as t]
             [clojure.edn :as edn]
-            [sails-forth :as sf]
+            [sails-forth.client :as sf]
             [sails-forth.query :as sq]))
 
 (defn build-client!
   [path]
-  (-> path slurp edn/read-string sf/build-client!))
+  (-> path slurp edn/read-string sf/build-http-client))
