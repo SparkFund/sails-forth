db53593cb509ec8efdd870551bc2f3ae5774d9fb
Donald Ball <donald.ball@gmail.com>
2016-07-24 23:04:04 -0400

    Update ns references

--- src/sails_forth/update.clj
+++ src/sails_forth/update.clj
@@ -1,10 +1,10 @@
 (ns sails-forth.update
   "Execute SalesForce updates using more idiomatic Clojure syntax."
   (:require [clojure.core.typed :as t]
-            [sails-forth :as sf]
+            [sails-forth.client :as sc]
             [sails-forth.clojurify :refer :all]))
 
-(t/ann update! [sf/SalesforceClient Keyword sf/SalesforceId (t/Map t/Keyword t/Any)])
+(t/ann update! [sc/SalesforceClient Keyword sc/SalesforceId (t/Map t/Keyword t/Any)])
 (defn update!
   "Performs updates on the object `object-id` with the given `type`."
   [client type object-id new-value-map]
@@ -20,12 +20,12 @@
     (when-not sf-type
       (throw (ex-info (str "no SalesForce type for " type)
                       {:description (get-type-description client type)})))
-    (sf/update! client
+    (sc/update! client
                 sf-type
                 object-id
                 sf-value-map)))
 
-(t/ann create! [sf/SalesforceClient Keyword (t/Map t/Keyword t/Any)])
+(t/ann create! [sc/SalesforceClient Keyword (t/Map t/Keyword t/Any)])
 (defn create!
   [client type new-value-map]
   (let [sf-type (:name (get-type-description client type))
@@ -40,6 +40,6 @@
     (when-not sf-type
       (throw (ex-info (str "no SalesForce type for " type)
                       {:description (get-type-description client type)})))
-    (sf/create! client
+    (sc/create! client
                 sf-type
                 sf-value-map)))
