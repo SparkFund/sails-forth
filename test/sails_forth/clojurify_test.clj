c7493be77784dc5d0299bbcabe549fe17487817f
Donald Ball <donald.ball@gmail.com>
2016-07-29 14:26:44 -0400

    Add render-value, use when updating from clojure

--- test/sails_forth/clojurify_test.clj
+++ test/sails_forth/clojurify_test.clj
@@ -40,6 +40,27 @@
   (testing "other"
     (is (= "foo" (parse-value {} "foo")))))
 
+(deftest test-render-value
+  (testing "datetime"
+    (is (= (render-value {:type "datetime"}
+                         (time/date-time 2015 1 1 12 30 15 500))
+           "2015-01-01T12:30:15.500Z"))
+    (is (= (render-value {:type "datetime"}
+                         "2015-01-01T12:30:15.500Z")
+           "2015-01-01T12:30:15.500Z")))
+  (testing "date"
+    (is (= (render-value {:type "date"}
+                         (time/local-date 2015 2 1))
+           "2015-02-01")))
+  (testing "percent"
+    (is (= (render-value {:type "percent"
+                          :scale 2
+                          :precision 4}
+                         0.0618M)
+           6.18M)))
+  (testing "other"
+    (is (= "foo" (parse-value {} "foo")))))
+
 (deftest ^:integration test-get-field-description
   (let [client (sf/build-http-client (test/load-config))]
     (is (get-field-description client :opportunity :id))
