d0dd987b59024a7e37a928922a0596390634b76b
Donald Ball <donald.ball@gmail.com>
2017-12-11 21:34:08 -0500

    Finish first version
    It asserts id fields and everything

--- src/sails_forth/datomic.clj
+++ src/sails_forth/datomic.clj
@@ -1,4 +1,5 @@
 (ns sails-forth.datomic
+  "Provides fns to assert the results of salesforce queries as datoms."
   (:require [clj-time.coerce :as tc]
             [clojure.set :as set]
             [clojure.string :as s]
@@ -21,41 +22,63 @@
    "picklist" :db.type/ref
    "url" :db.type/uri
    "multipicklist" :db.type/ref
-   ;; TODO component? Does this have discrete pieces?
+   ;; maybe components? Do these have discrete pieces?
    ;; "address" :db.type/string
-   ;; TODO component? Same as address
    ;; "phone" :db.type/string
    })
 
+(defn field-ident
+  [ns-prefix field-name]
+  (keyword (str (name ns-prefix) ".field") (name field-name)))
+
+(defn field-attr
+  [ns-prefix object-name field-name]
+  (keyword (str (name ns-prefix) ".object." (name object-name)) (name field-name)))
+
+(defn enum-ident
+  [ns-prefix object-name field-name value]
+  (keyword (str (name ns-prefix) ".object." (name object-name) "." (name field-name)) (name value)))
+
 (defn metadata-schema
   [ns-prefix]
-  ;; TODO there are so many other salesforce schema metadata
-  (let [ns-prefix (name ns-prefix)
-        name-md (keyword ns-prefix "name")
-        type-md (keyword ns-prefix "type")
-        formula-md (keyword ns-prefix "formula")
-        helptext-md (keyword ns-prefix "helptext")
-        picklist-value-md (keyword ns-prefix "picklist-value")]
-    [{:db/ident name-md
-      :db/doc "Salesforce field name"
-      :db/valueType :db.type/string
-      :db/cardinality :db.cardinality/one}
-     {:db/ident type-md
-      :db/doc "Salesforce field type"
-      :db/valueType :db.type/string
-      :db/cardinality :db.cardinality/one}
-     {:db/ident formula-md
-      :db/doc "Salesforce field formula"
-      :db/valueType :db.type/string
-      :db/cardinality :db.cardinality/one}
-     {:db/ident helptext-md
-      :db/doc "Salesforce help text"
-      :db/valueType :db.type/string
-      :db/cardinality :db.cardinality/one}
-     {:db/ident picklist-value-md
-      :db/doc "Salesforce picklist value"
-      :db/valueType :db.type/string
-      :db/cardinality :db.cardinality/one}]))
+  ;; More field metadata could come along for the ride
+  [{:db/ident (field-ident ns-prefix "name")
+    :db/doc "Salesforce field name"
+    :db/valueType :db.type/string
+    :db/cardinality :db.cardinality/one}
+   {:db/ident (field-ident ns-prefix "type")
+    :db/doc "Salesforce field type"
+    :db/valueType :db.type/string
+    :db/cardinality :db.cardinality/one}
+   {:db/ident (field-ident ns-prefix "formula")
+    :db/doc "Salesforce field formula"
+    :db/valueType :db.type/string
+    :db/cardinality :db.cardinality/one}
+   {:db/ident (field-ident ns-prefix "helptext")
+    :db/doc "Salesforce field help text"
+    :db/valueType :db.type/string
+    :db/cardinality :db.cardinality/one}
+   {:db/ident (field-ident ns-prefix "picklist-value")
+    :db/doc "Salesforce field picklist value"
+    :db/valueType :db.type/string
+    :db/cardinality :db.cardinality/one}])
+
+(def picklist-name
+  (let [allowed-punct (into #{} "*+!-_'?")]
+    (fn [picklist-value]
+      (let [sb (StringBuffer.)]
+        (doseq [c (seq picklist-value)]
+          (cond (and (zero? (.length sb))
+                     (Character/isDigit c))
+                nil
+                (or (Character/isLetterOrDigit c)
+                    (contains? allowed-punct c))
+                (.append sb (Character/toLowerCase c))
+                :else
+                (.append sb \-)))
+        (-> (.toString sb)
+            (s/replace #"-+" "-")
+            (s/replace #"^-|-$" ""))))))
 
 (defn object-schema
   [ns-prefix object-key fields]
@@ -65,14 +88,9 @@
               (letfn [(enum-datom [item]
                         (let [ns (str ns "." (name key))
                               {:keys [label value]} item]
-                          ;; TODO value may not become a keyword that can be
-                          ;; read by the clj/edn reader. That's annoying.
-                          ;; Whatever we choose to do here must be understood
-                          ;; by our readers and writers
-                          {:db/ident (keyword ns value)
-                           ;; TODO are docstrings allowed on non-attributes?
+                          {:db/ident (enum-ident ns-prefix object-key key (picklist-name value))
                            :db/doc label
-                           (keyword ns-prefix "picklist-value") value}))]
+                           (field-ident ns-prefix "picklist-value") value}))]
                 (map enum-datom (:picklistValues field))))
             (field-datoms [[key field]]
               (let [{:keys [name
@@ -85,19 +103,19 @@
                     cardinality (if (= "multipicklist" type)
                                   :db.cardinality/many
                                   :db.cardinality/one)]
-                (cons (cond-> {:db/ident (keyword ns (clojure.core/name key))
+                (cons (cond-> {:db/ident (field-attr ns-prefix object-key key)
                                :db/doc label
                                :db/valueType (get datomic-types type)
                                :db/cardinality cardinality
-                               (keyword ns-prefix "name") name
-                               (keyword ns-prefix "type") type}
+                               (field-ident ns-prefix "name") name
+                               (field-ident ns-prefix "type") type}
                         ;; Sorta funny that id types don't have :unique true
                         (or (= "id" type) unique)
                         (assoc :db/unique :db.unique/identity)
                         calculatedFormula
-                        (assoc (keyword ns-prefix "formula") calculatedFormula)
+                        (assoc (field-ident ns-prefix "formula") calculatedFormula)
                         inlineHelpText
-                        (assoc (keyword ns-prefix "helptext") inlineHelpText))
+                        (assoc (field-ident ns-prefix "helptext") inlineHelpText))
                       (when (case type
                               "picklist" true
                               "multipicklist" true
@@ -107,10 +125,11 @@
 
 (defn build-schema!
   [client ns-prefix object-keys]
-  (into (metadata-schema ns-prefix)
-        (mapcat (fn [object-key]
-                  (object-schema ns-prefix object-key (c/get-fields client object-key))))
-        object-keys))
+  [(metadata-schema ns-prefix)
+   (into []
+         (mapcat (fn [object-key]
+                   (object-schema ns-prefix object-key (c/get-fields client object-key))))
+         object-keys)])
 
 (defn assert-object!
   [client ns-prefix object-key m]
@@ -118,16 +137,17 @@
         ns (str ns-prefix "." (name object-key))
         fields (c/get-fields client object-key)]
     (reduce-kv (fn [txn field-key value]
-                 (let [attr (keyword ns (name field-key))
+                 (let [attr (field-attr ns-prefix object-key field-key)
                        field (get fields field-key)
                        {:keys [type referenceTo]} field
                        [value ref-types]
                        (case type
-                         ;; changes must be coordinated with enum-datom
                          "picklist"
-                         [(keyword (str ns "." (name field-key)) value)]
+                         (enum-ident ns-prefix object-key field-key (picklist-name value))
                          "multipicklist"
-                         [(mapv (partial keyword (str ns "." (name field-key)))
+                         [(into []
+                                (map (comp (partial enum-ident ns-prefix object-key field-key)
+                                           picklist-name))
                                 (s/split value #";"))]
                          "date"
                          [(tc/to-date value)]
@@ -144,15 +164,32 @@
                {::types #{object-key}}
                m)))
 
-;; TODO also do we want the whole schema for each type or just projections that
-;; permit out query results
 (defn assert-query!
+  "Returns a seq of transaction seqs that if transacted in order will assert
+   the results of the given query in a datomic database.
+
+   Given an ns-prefix of `ex` and a query of `{:find [:customer :id :sectors]}`
+
+   The first transaction asserts a set of attributes that will be defined on the
+   attributes that will model the salesforce fields where there is no direct
+   datomic analog, e.g. `ex.field/helptext`.
+
+   The second transaction asserts a set of attributes that model the
+   fields of objects used in the query,
+   e.g. `ex.object.customer/id`. Note this is a complete set of
+   attributes, not limited simply to those used in the query.
+
+   The last transaction asserts the entities returned by the query.
+
+   Most field values have natural datomic types. Notably, however, picklist
+   and multipicklist fields are modeled as enums whose idents are a
+   keywordized version of the picklist value with a namespace derived from
+   the object and field, e.g. `ex.object.customer.sectors/energy-efficiency`."
   [client ns-prefix query]
   (let [objects (into []
                       (comp (map (comp first seq))
                             (map (partial apply assert-object! client ns-prefix)))
                       (q/query client query))
         object-keys (reduce set/union #{} (map ::types objects))]
-    (into (build-schema! client ns-prefix object-keys)
-          (map (fn [m] (dissoc m ::types))
-               objects))))
+    (conj (build-schema! client ns-prefix object-keys)
+          (into [] (map (fn [m] (dissoc m ::types)) objects)))))
